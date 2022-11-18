bd157328154cf4483abfffb6f55a54662265e35e
shalini-polapally <37867151+shalini-polapally@users.noreply.github.com>
2022-11-18 13:19:51 +0530

    1 file modified, 1 deleted

--- src/app.ts
+++ src/app.ts
@@ -32,11 +32,12 @@ const getPokemon = async (id: number): Promise<void> => {
 
 const showPokemon = (pokemon: Ipokemon): void => {
   let output: string = `<div class="card">
-  <span class = "card--id">#${pokemon.id}</span>
+  <span class ="card--id">#${pokemon.id}</span>
   <img class="card--image" src=${pokemon.image} alt=${pokemon.name}/>
   <h1 class="card--name">${pokemon.name}</h1>
   <span class="card--details">${pokemon.type}</span>
   </div>`;
+  container.innerHTML += output;
 };
 
 fetchData();
