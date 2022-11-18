852443d1a207a699f84b93746a7c49d8ebb7c0d1
shalini-polapally <37867151+shalini-polapally@users.noreply.github.com>
2022-11-18 12:52:56 +0530

    1 file modified

--- src/app.ts
+++ src/app.ts
@@ -15,5 +15,28 @@ const fetchData = (): void => {
 };
 
 const getPokemon = async (id: number): Promise<void> => {
-  const data: Response = await fetch(``);
+  const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const pokemon: any = await data.json();
+  const pokemonType: string = pokemon.types
+    .map((poke: any) => poke.type.name)
+    .join(', ');
+
+  const transformedPokemon = {
+    id: pokemon.id,
+    name: pokemon.name,
+    image: `${pokemon.sprites.front_default}`,
+    type: pokemonType,
+  };
+  showPokemon(transformedPokemon);
 };
+
+const showPokemon = (pokemon: Ipokemon): void => {
+  let output: string = `<div class="card">
+  <span class = "card--id">#${pokemon.id}</span>
+  <img class="card--image" src=${pokemon.image} alt=${pokemon.name}/>
+  <h1 class="card--name">${pokemon.name}</h1>
+  <span class="card--details">${pokemon.type}</span>
+  </div>`;
+};
+
+fetchData();
